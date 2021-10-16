917728f4cb75407c35b7eed90a03d0ce20314728
Quyen <qtruongk@gmail.com>
2021-10-16 23:09:58 +0000

    Square Added

--- arithmetic.py
+++ arithmetic.py
@@ -26,7 +26,8 @@ def divide(num1, num2):
 
 def square(num1):
     """Return the square of the input."""
-
+    square = num1 ** 2
+    return square
 
 def cube(num1):
     """Return the cube of the input."""
