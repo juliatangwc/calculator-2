dd2b70c704a46f80cf5e7c8e0610d4c1f1497e66
Quyen <qtruongk@gmail.com>
2021-10-16 23:06:39 +0000

    Multiplication and Subtraction Added

--- arithmetic.py
+++ arithmetic.py
@@ -16,7 +16,8 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    multiply = num1 * num2
+    return multiply
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
